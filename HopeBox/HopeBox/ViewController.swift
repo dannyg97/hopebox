9403c3caa3ef54bc845c41ec89ce8664b0234be6
Chengbin <kfengc27@gmail.com>
2019-10-20 12:33:32 +1100

    init the project files

---
+++ HopeBox/HopeBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HopeBox
+//
+//  Created by Chengbin Feng on 20/10/19.
+//  Copyright © 2019 Chengbin Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
