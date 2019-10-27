698a746ec467672aafc09610d4a98e4dcd87a832
Chengbin <kfengc27@gmail.com>
2019-10-27 11:55:54 +1100

    add files

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,22 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-#import <cloud_firestore/CloudFirestorePlugin.h>
-#import <firebase_analytics/FirebaseAnalyticsPlugin.h>
-#import <firebase_auth/FirebaseAuthPlugin.h>
-#import <firebase_core/FirebaseCorePlugin.h>
-#import <firebase_database/FirebaseDatabasePlugin.h>
-#import <leancloud_flutter_plugin/LeancloudFlutterPlugin.h>
+
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTCloudFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTCloudFirestorePlugin"]];
-  [FLTFirebaseAnalyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAnalyticsPlugin"]];
-  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
-  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
-  [FLTFirebaseDatabasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseDatabasePlugin"]];
-  [LeancloudFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"LeancloudFlutterPlugin"]];
+
 }
 
 @end
