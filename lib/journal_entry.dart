1de0bd3d3b4f92ea732aebcb1dbbd28bbf2f7b0a
Danny <38267274+dannyg97@users.noreply.github.com>
2019-10-31 18:00:06 +1100

    add basic save to file + code refactoring

--- lib/journal_entry.dart
+++ lib/journal_entry.dart
@@ -5,9 +5,24 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 void main() => runApp(JournalEntry());
 
+/*
+
+ TODO:
+ Test the functionality of:
+   [DONE] Writing anything should 'echo' inside the textbox
+   [DONE] Tapping the save button should: save and erase what is currently on the screen
+
+ Ideally:
+  Have a basic journal entry page that lists all entries (O.K. if just one)
+  [column]
+  |-[curved square]
+      |-[text]
+ */
 /// This Widget is the main application widget.
 class JournalEntry extends StatelessWidget {
   static const String _title = 'What\'s on your mind?';
@@ -16,74 +31,112 @@ class JournalEntry extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: Scaffold(
-        appBar: new AppBar(
-          title: const Text(_title),
-          backgroundColor: const Color(0xFFFADA5E),
-          leading: new IconButton(icon: new Icon(Icons.arrow_back)),
-          actions: [new IconButton(icon: new Icon(Icons.done),
-                onPressed: () {
-                  // Navigate to second route when tapped.
-                  Navigator.pop(context);
-                  })
-              ]
-        ),
-        body: MyStatefulWidget(),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: MyStatefulWidget()
     );
   }
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final _formKey = GlobalKey<FormState>();
+//  final _formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-          Padding(
-          padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              focusNode: FocusNode(),
-              controller: TextEditingController(),
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              decoration: InputDecoration.collapsed(
-                hintText: 'Start typing...',
-                hintStyle: TextStyle(
-                    color: Colors.grey.shade400,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500),
-                border: InputBorder.none,
+    return Scaffold(
+        appBar: new AppBar(
+            title: const Text("What's on your mind?"),
+            backgroundColor: const Color(0xFFFADA5E),
+            leading: new IconButton(icon: new Icon(Icons.arrow_back)),
+            actions: [new IconButton(icon: new Icon(Icons.done),
+                onPressed: () {
+                  var input = myController.text;
+                  myController.clear();
+                  _save(input);
+                  return showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                            content: Text(input)
+                        );
+                      }
+                  );
+                })
+            ]
+        ),
+        body: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextField(
+                  focusNode: FocusNode(),
+                  controller: myController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Write about your day',
+                    hintStyle: TextStyle(
+                        color: Colors.grey.shade400,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                    border: InputBorder.none,
+                  ),
+                ),
               ),
-            ),
+              /*
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState.validate()) {
+                        // Process data.
+                        print(_formKey.toString());
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
+                ),
+
+         */
+            ],
           ),
-//          Padding(
-//            padding: const EdgeInsets.all(16.0),
-//            child: RaisedButton(
-//              onPressed: () {
-//                // Validate will return true if the form is valid, or false if
-//                // the form is invalid.
-//                if (_formKey.currentState.validate()) {
-//                  // Process data.
-//                }
-//              },
-//              child: Text('Submit'),
-//            ),
-//          ),
-        ],
-      ),
-    );
+        ));
   }
+}
+
+
+//_read() async {
+//  try {
+//    final directory = await getApplicationDocumentsDirectory();
+//    final file = File('${directory.path}/my_file.txt');
+//    String text = await file.readAsString();
+//    print(text);
+//  } catch (e) {
+//    print("Couldn't read file");
+//  }
+//}
+
+_save(var input) async {
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/my_file.txt');
+  final text = input;
+  await file.writeAsString(text);
+  print(directory);
+  print('File has been saved');
 }
