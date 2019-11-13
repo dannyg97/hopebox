ebba468f0013de0bb8d99e2f150f0cb2967580b5
Danny <danny.guo97@gmail.com>
2019-11-14 10:43:12 +1100

    firebase is now android compatible

--- build/ios/Debug-iphonesimulator/firebase_auth/firebase_auth.framework/Headers/UserAgent.h
+++
@@ -1,3 +0,0 @@
-// Generated file, do not edit
-#define LIBRARY_VERSION @"0.14.0-5"
-#define LIBRARY_NAME @"flutter-fire-auth"
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/kfengc27/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kfengc27/Developer/hopebox"
-export "FLUTTER_TARGET=/Users/kfengc27/Developer/hopebox/lib/main.dart"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Chibi\hopebox"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/kfengc27/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
