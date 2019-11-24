37e95f17599b40e286e816e844b52f0a4432ac1c
Kevin Feng <kfengc27@gmail.com>
2019-11-24 15:45:29 +1100

    make logout done

--- lib/account/information.dart
+++ lib/account/information.dart
@@ -1,43 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/registration/authentication.dart';
 
 class AccountPage extends StatelessWidget {
+
+  AccountPage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+  
+  
+  signOut() async {
+    try {
+      await this.auth.signOut();
+      this.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Column(
-       children: <Widget>[
-         Padding(
-           padding: EdgeInsets.all(10.0),
-         ),
-         Align(
-          alignment: Alignment(0.0, -0.8),
-          child: Avator(),
-        ),
-        Padding(
-           padding: EdgeInsets.all(3.0),
-         ),
-        Text(
-          'User Name',
-          style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () { 
+              signOut();
+          },
+          child: const Text(
+            'Logout',
+            style: TextStyle(fontSize: 20)
           ),
         ),
-        Expanded(
-            child: Divider(
-               color: Colors.black,
-               height: 36,
-            )
-        ), 
-       ],
-      
+          ],
+        )
+        
       )
     );
   }
 }
-
 
 Widget Avator()=>Stack (
       alignment: const Alignment(0.6, 0.6),
@@ -46,6 +54,6 @@ Widget Avator()=>Stack (
           backgroundImage: AssetImage(''),
           radius: 80,
         ),
-       
+
       ],
-    );
+);
